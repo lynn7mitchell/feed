0247b00fc106e87dadc0b618fe1ded2d544c1aa7
lynn7mitchell <lynn7mitchell@gmail.com>
2020-04-27 21:09:01 -0500

    added add post modal button

--- client/src/components/DesktopNavbar/DesktopNavbar.js
+++ client/src/components/DesktopNavbar/DesktopNavbar.js
@@ -7,10 +7,10 @@ export default function DesktopNavbar() {
       <h4>FEED</h4>
       <div><input type="text" placeholder="Search"/></div>
       <div className="nav-icons">
-        <i class="material-icons">account_circle</i>
-        <i class="material-icons">notifications</i>
-        <i class="material-icons">sms</i>
-        <i class="material-icons">settings</i>
+        <i className="material-icons">account_circle</i>
+        <i className="material-icons">notifications</i>
+        <i className="material-icons">sms</i>
+        <i className="material-icons">settings</i>
       </div>
     </nav>
   );
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import './dashboard.css'
-import DesktopNavbar from "../../components/DesktopNavbar/DesktopNavbar"
+import "./dashboard.css";
+import DesktopNavbar from "../../components/DesktopNavbar/DesktopNavbar";
+import AddPostModal from "../../components/AddPostModal/AddPostModal";
 import setAuthToken from "../../utils/setAuthtoken";
 import axios from "axios";
 
 export class Dashboard extends Component {
   state = {
-    user: {}
+    user: {},
   };
   componentWillMount() {
     const token = localStorage.getItem("example-app");
@@ -18,30 +19,46 @@ export class Dashboard extends Component {
 
     axios
       .get("api/user")
-      .then(response => {
+      .then((response) => {
         this.setState({
-          user: response.data
+          user: response.data,
         });
       })
-      .catch(err => console.log(err.response));
+      .catch((err) => console.log(err.response));
   }
+
+
   handleLogout = () => {
     localStorage.removeItem("example-app");
     this.setState({
-      redirect: true
+      redirect: true,
     });
   };
+
+
+
+
+
+   startNewPost = (e) => {
+    e.preventDefault()
+    console.log("clicked")
+  }
+
+
   render() {
     return (
       <div>
-        <DesktopNavbar/>
+        <DesktopNavbar />
+        <AddPostModal/>
         {/* <i className="material-icons account-icon">account_circle</i> */}
         <Link to="/">
           <button className="logout-button" onClick={this.handleLogout}>
             Log Out
           </button>
         </Link>
-        <h1>Dashboard</h1>
+        <button className="floating-add-post-button" onClick={(e)=>{this.startNewPost(e)}}>
+          <i className="material-icons">add</i>
+        </button>
       </div>
     );
   }
