901f288a31f32d49d07ea78a8f9fb8f1d900d495
lynn7mitchell <lynn7mitchell@gmail.com>
2020-08-13 16:15:16 -0500

    added posts to dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -2,32 +2,47 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./dashboard.css";
 import DesktopNavbar from "../../components/DesktopNavbar/DesktopNavbar";
-import MobileNavbar from "../../components/MobileNavbar/MobileNavbar"
+import MobileNavbar from "../../components/MobileNavbar/MobileNavbar";
 import AddPostForm from "../../components/AddPostForm/AddPostForm";
+import PostCard from '../../components/PostCard/PostCard'
 import setAuthToken from "../../utils/setAuthtoken";
 import axios from "axios";
 
 export class Dashboard extends Component {
   state = {
     user: {},
+    posts: '',
   };
   componentWillMount() {
     const token = localStorage.getItem("example-app");
-
+    const thisUser = {};
     if (token) {
       setAuthToken(token);
     }
 
     axios
       .get("api/user")
-      .then((response) => {
+      .then((res) => {
         this.setState({
-          user: response.data,
+          user: res.data,
+        });
+        thisUser = res.data
+      })
+      .catch((err) => console.log(err.res));
+
+    axios
+      .get("/allPosts")
+      .then((res) => {
+        this.setState({
+          posts: (
+            <div>
+              <PostCard userPosts={res.data} currentLoggedInUser={thisUser} />
+            </div>
+          ),
         });
       })
-      .catch((err) => console.log(err.response));
+      .catch((err) => console.log(err.res));
   }
-
 
   handleLogout = () => {
     localStorage.removeItem("example-app");
@@ -36,21 +51,17 @@ export class Dashboard extends Component {
     });
   };
 
-
-
-
-
-   startNewPost = (e) => {
-    e.preventDefault()
-    console.log("clicked")
-  }
-
+  startNewPost = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
 
   render() {
     return (
       <div className="dashboard">
-        <DesktopNavbar user={this.state.user}/>
-        <AddPostForm user={this.state.user}/>
+        <DesktopNavbar user={this.state.user} />
+        <AddPostForm user={this.state.user} />
+        {this.state.posts}
         {/* <i className="material-icons account-icon">account_circle</i> */}
         <Link to="/">
           <button className="logout-button" onClick={this.handleLogout}>
@@ -60,7 +71,7 @@ export class Dashboard extends Component {
         {/* <button className="floating-add-post-button" onClick={(e)=>{this.startNewPost(e)}}>
           <i className="material-icons">add</i>
         </button> */}
-        <MobileNavbar user={this.state.user}/>
+        <MobileNavbar user={this.state.user} />
       </div>
     );
   }
