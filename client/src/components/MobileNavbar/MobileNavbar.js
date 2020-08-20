4f38ffbaf6bdc71d816bd840266c5cc3d468fbf4
lynn7mitchell <lynn7mitchell@gmail.com>
2020-08-19 20:44:56 -0500

    added working mobile search to mobile navbar

--- client/src/components/MobileNavbar/MobileNavbar.js
+++ client/src/components/MobileNavbar/MobileNavbar.js
@@ -4,25 +4,123 @@ import "./mobile-navbar.css";
 import axios from "axios";
 export default function MobileNavbar(user) {
   const [users, setUsers] = useState([]);
-  const [currentUser, setCurrentUser] = useState({});
+  const [currentUser, setCurrentUser] = useState(user);
+  const [searchIsOpen, setSearchIsOpen] = useState(false);
+  const [searchSuggestions, setSearchSuggestions] = useState([]);
 
   useEffect(() => {
     axios
       .get("/api/allUsers")
       .then((res) => {
-        console.log(res.data);
+        setUsers(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
-  return (
-    <div className="mobile-navbar">
-      <Link to={'/dashboard'}>
+
+  const onChange = (e) => {
+    // because this variable is set to have nothing in it on every onChange
+    // you do not have to loop through the searchSuggestions to get rid of suggestions
+    // that do not include e.target.value
+    console.log(e.target.value);
+    let newSuggestions = [];
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username.includes(e.target.value)) {
+        newSuggestions.push(users[i]);
+      }
+    }
+    document.getElementById('search-suggestions').style.display = 'block'
+    setSearchSuggestions(newSuggestions);
+
+    if(newSuggestions.length === users.length){
+      document.getElementById('search-suggestions').style.display = 'none'
+    }
+  };
+  
+
+  let homeButton = (
+    <Link to={"/dashboard"}>
       <i className="material-icons">home</i>
-      </Link>
-      <i className="material-icons">search</i>
-      <Link to={`/profile/${user.user.username}`}>
+    </Link>
+  );
+
+  let searchButton = (
+    <i
+      className="material-icons"
+      onClick={(e) => {
+        openMobileSearch(e);
+      }}
+    >
+      search
+    </i>
+  );
+
+  let profileButton= (
+<Link to={`/profile/${user.user.username}`}>
         <i className="material-icons">account_circle</i>
       </Link>
+  )
+  if (searchIsOpen) {
+
+    if(window.location.href.includes('dashboard')){
+    homeButton = (
+      <i className="material-icons" onClick={(e) => exitSearch(e)}>
+        home
+      </i>
+    )}
+
+    if(window.location.href.includes('profile')){
+      profileButton = (
+        <i className="material-icons" onClick={(e) => exitSearch(e)}>
+          account_circle
+        </i>
+      )}
+    searchButton = (
+      <i
+        className="material-icons"
+        onClick={(e) => {
+          exitSearch(e);
+        }}
+      >
+        search
+      </i>
+    );
+  }
+
+  const openMobileSearch = (e) => {
+    document.getElementById("mobile-search").style.display = "block";
+    setSearchIsOpen(true);
+  };
+
+  const exitSearch = (e) => {
+    document.getElementById("mobile-search").style.display = "none";
+    setSearchIsOpen(false);
+  };
+  return (
+    <div className="mobile-navbar">
+      <div id="mobile-search">
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => onChange(e)}
+          />
+          <div id="search-suggestions">
+            {searchSuggestions.map((searchSuggestion) => {
+              return (
+                <Link
+                  to={"/profile/" + searchSuggestion.username}
+                  style={{ display: "block" }}
+                >
+                  {searchSuggestion.username}
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      {homeButton}
+      {searchButton}
+      {profileButton}
       <i className="material-icons">sms</i>
       <i className="material-icons">notifications</i>
     </div>
